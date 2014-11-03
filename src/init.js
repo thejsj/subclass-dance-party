fd72ec1b0336279bcf657777837ad035585a0467
thejsj <jorge.silva@thejsj.com>
2014-11-03 14:51:29 -0800

    refactor Dancer and BlinkyDancer to pseudoclassical

--- src/init.js
+++ src/init.js
@@ -15,14 +15,14 @@ $(document).ready(function(){
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
+    var dancerConstructorName = $(this).data("dancer-maker-function-name");
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancerConstructor = window[dancerConstructorName];
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerConstructor(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
